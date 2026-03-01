a1719aa0f7d2ebe75f2b0460c4ce0a7a5536802e
Wilbur Jaywright <zargulthewizard@outlook.com>
2026-02-28 22:41:08 -0500

    Preparing for release
    - Improved documentation
    - Changed input to be stdin instead of wordlist.txt
    - Cleared out disabled lines
    - Made errors go to stderr instead of stdout
    - Slight performance improvement in loop reset
    
    S.D.G.

--- bwparser.c
+++ bwparser.c
@@ -1,13 +1,24 @@
+/* BookWorm Deluxe wordlist parser
+ *
+ * wordlist.txt contents are piped to stdin,
+ * and parsed words are printed to stdout, one per line.
+ *
+ * Copyright 2026, Wilbur Jaywright dba Marswide BGL.
+ *
+ * All rights reserved.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
+// Read directly from input
+#define wordlist stdin
+
 int main() {
   int copy_chars = 0;  // Characters to copy from previous word
   int copy_chars_strlen = 0;  // How long IN characters the copy characters specifier of each entry was
-  FILE *wordlist = fopen("wordlist.txt", "r");  // The input file
-  // FILE *wordlist_parsed = fopen("wordlist_parsed.txt", "w");
   char entry[99] = "";  // Each line of the file at a time 
   char word_ending[99] = "";  // The part of each entry that is the end of the new word
   char next_word[99] = "";  // Each new word as it is parsed
@@ -29,12 +40,11 @@ int main() {
       entry[strlen(entry) - 1] = '\0';
     }
 
-    // printf("ENTRY: '%s'\n", entry);
-
     // Look for numbers at the beginning of the string
     copy_chars_strlen = 0;
     for (int i=0; i<strlen(entry) && isdigit(entry[i]); i++) {
-      // The character is an integer if we are even in this loop
+      // NOTE: No part of this loop will execute if the first character is not an integer
+
       // This is the first character? reset copy chars
       if (i == 0) {
         copy_chars = 0;
@@ -50,20 +60,18 @@ int main() {
       word_ending[j - copy_chars_strlen] = entry[j];
     }
 
-    // printf("COPY CHARS: %d\tWORD ENDING: '%s'\n", copy_chars, word_ending);
-
     // Do character copying
     if (copy_chars != 0) {
 
       // Erroneous presence of copy characters with nothing to copy
       // The start of last_word is null terminator
       if (*last_word == '\0') {
-        printf("WARNING: Copy characters was nonzero at start of file. Ignoring.\n");
+        fprintf(stderr, "WARNING: Copy characters was nonzero at start of file. Ignoring.\n");
       }
 
       // Not enough characters to copy
       else if (strlen(last_word) < copy_chars) {
-        printf("ERROR: Entry '%s' has copy character characters quirement %d but previous entry '%s' is too short!",
+        fprintf(stderr, "ERROR: Entry '%s' has copy character characters requirement %d but previous entry '%s' is too short!\n",
                entry, copy_chars, last_word);
         exit(EXIT_FAILURE);
       }
@@ -82,12 +90,17 @@ int main() {
     // Output
     printf("%s\n", next_word);
 
-    // Reset stuff for next iteration
+    // Save the last word
     strcpy(last_word, next_word);
+
+    // These strings will be set by character iteration, so we must fully clear them.
     memset(next_word, '\0', sizeof(next_word));
-    memset(entry, '\0', sizeof(entry));
     memset(word_ending, '\0', sizeof(word_ending));
-    // fgets(entry, 99, wordlist);
+
+    /* fgets won't write anything to the entry buffer when it reaches EOF,
+     * not even a null terminator. So, we must quick clear that.
+     */
+    entry[0] = '\0';
   }
   
   return 0;
